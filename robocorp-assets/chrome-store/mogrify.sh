0ef14bd6090da28108a3cac395b9a2eb1597f946
Kerkko Pelttari <kerkko@robocorp.com>
2021-01-13 12:15:23 +0200

    Update mogrify script, use better background color for chrome store images

--- robocorp-assets/chrome-store/mogrify.sh
+++ robocorp-assets/chrome-store/mogrify.sh
@@ -1,1 +1,4 @@
-mogrify -resize 1280x800 -background black -gravity center -extent 1280x800 -quality 100 image.png
+#!/bin/sh
+mogrify -resize 1280x800 -background '#F0F0F0' -gravity center -extent 1280x800 -quality 100 main-view.png
+mogrify -resize 1280x800 -background '#F0F0F0' -gravity center -extent 1280x800 -quality 100 scan-output.png
+mogrify -resize 1280x800 -background '#F0F0F0' -gravity center -extent 1280x800 -quality 100 settings-view.png
